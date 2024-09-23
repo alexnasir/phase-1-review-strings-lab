c279738b246c562c405910b66ee9661696180c67
alex <alexnasiali45@gmail.com>
2024-09-23 14:04:09 +0300

    intial commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code in this file!
+const currentUser = 'Grace Hopper';
+console.log(currentUser);
+const welcomeMessage = 'Welcome to Flatbook,  Grace Hopper!';
+console.log(welcomeMessage)
+let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, $ {GRACE HOPPER}!';
+console.log(excitedWelcomeMessage.toUpperCase())
+const shortGreeting = 'Welcome, $ {G}!';
+console.log(shortGreeting)
+
